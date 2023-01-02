39c8dbf895d5abebf7e2cea7fa593bd6db353eea
Bartanakin <bartanakin@interia.pl>
2023-01-02 14:24:09 +0100

    init-dev1

--- migrations-db.php
+++ migrations-db.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'dbname' => 'chocolate_factory',
-//    'dbname' => 'chocolate_factory_snap1',
+//    'dbname' => 'chocolate_factory',
+    'dbname' => 'chocolate_factory_snap1',
 //    'dbname' => 'chocolate_factory_snap2',
     'user' => 'root',
     'password' => 'root',
-    'host' => 'localhost:3310',
-//    'host' => 'localhost:3311',
+//    'host' => 'localhost:3310',
+    'host' => 'localhost:3311',
     'driver' => 'pdo_mysql',
 ];
